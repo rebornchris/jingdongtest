9f4dc6b606a0ce204eae0ea2f2abc77765d001a4
rebornchris <415817317@qq.com>
2016-10-21 23:31:02 +0800

    完成静态页面，动画还没实现

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-window.onload = function(){
+window.onload = function() {
     main.app.disappear();
     main.app.pullDown();
     main.app.userBuy();
     main.app.imageChange();
     main.app.tap();
+    main.app.productList();
 };
-var main={};
+var main = {};
 
 main.app = {};
 
-main.app.pullDown = function(){
+main.app.pullDown = function() {
     var aPull = document.getElementsByClassName('pullDown');
     var aPullList = document.getElementsByClassName('pdList');
     var aArrow = document.getElementsByClassName('arrow');
 
-    for(var i = 0; i<aPull.length;i++){
-        (function(i){
-            aPull[i].onmouseover = function(){
+    for (var i = 0; i < aPull.length; i++) {
+        (function(i) {
+            aPull[i].onmouseover = function() {
                 this.style.backgroundColor = '#fff';
                 aPullList[i].style.display = 'block';
                 aArrow[i].style.backgroundImage = 'url(img/arrow1.gif)';
             }
-            aPull[i].onmouseout = function(){
+            aPull[i].onmouseout = function() {
                 this.style.backgroundColor = '#f1f1f1';
                 aPullList[i].style.display = 'none';
                 aArrow[i].style.backgroundImage = 'url(img/arrow2.gif)';
@@ -30,35 +31,35 @@ main.app.pullDown = function(){
     }
 }
 
-main.app.disappear = function(){
+main.app.disappear = function() {
     var ad = document.getElementById('top_ad_wrap');
     var close = document.getElementById('close');
 
-    close.addEventListener('click',function(){
+    close.addEventListener('click', function() {
         ad.classList.add('out');
-    },false);
+    }, false);
 
-    ad.addEventListener('transitionend',function(){
+    ad.addEventListener('transitionend', function() {
         ad.style.display = 'none';
-    },false);
+    }, false);
 };
 
-main.app.userBuy = function(){
+main.app.userBuy = function() {
     var oBuy = document.getElementById('user_pay');
     var oDivide = oBuy.getElementsByClassName('divide')[0];
     var oPullDown = document.getElementById('user_pay_pullDown');
-    oBuy.onmouseover = function(){
-		oDivide.style.display = 'block';
-		oPullDown.style.display = 'block';
-		}
-	oBuy.onmouseout = function(){
-		oDivide.style.display = 'none';
-		oPullDown.style.display = 'none';
-		}
+    oBuy.onmouseover = function() {
+        oDivide.style.display = 'block';
+        oPullDown.style.display = 'block';
+    }
+    oBuy.onmouseout = function() {
+        oDivide.style.display = 'none';
+        oPullDown.style.display = 'none';
+    }
 }
 
 // 轮播图
-main.app.imageChange= function(){
+main.app.imageChange = function() {
     var flash = document.getElementById('flash');
     var pic = document.getElementById('pic');
     var PList = pic.getElementsByTagName('li');
@@ -72,38 +73,38 @@ main.app.imageChange= function(){
     //点击左箭头
 
 
-    left.onclick = function(){
-        index-- ;
-        if(index < 0 ){
-            index = 5 ;
+    left.onclick = function() {
+        index--;
+        if (index < 0) {
+            index = 5;
         }
         change(index);
     }
 
     //点击右箭头
 
-    right.onclick = function(){
-        index++ ;
-        if(index>=numList.length){
+    right.onclick = function() {
+        index++;
+        if (index >= numList.length) {
             index = 0;
         }
         change(index);
     }
 
     //当鼠标停留在图片上面时候，停止计时器
-    flash.onmouseover = function(){
+    flash.onmouseover = function() {
         clearInterval(timer);
     }
 
     //当鼠标离开图片时候，开始计时器
-    flash.onmouseout = function(){
-        timer = setInterval(run,2000);
+    flash.onmouseout = function() {
+        timer = setInterval(run, 2000);
     }
 
     //当鼠标停留在页签上时，停止计时器并手动切换
-    for(var i = 0; i<numList.length; i++){
+    for (var i = 0; i < numList.length; i++) {
         numList[i].a = i;
-        numList[i].onmouseover = function(){
+        numList[i].onmouseover = function() {
             clearInterval(timer);
             change(this.a);
             //这里卡了好久 忘记change()传的参数是number
@@ -111,13 +112,13 @@ main.app.imageChange= function(){
         }
     }
     //定义计时器
-    timer = setInterval(run,2000);
+    timer = setInterval(run, 2000);
 
     //封装函数run
 
-    function run(){
+    function run() {
         index++;
-        if(index>=numList.length){
+        if (index >= numList.length) {
             index = 0;
         }
         change(index);
@@ -125,8 +126,8 @@ main.app.imageChange= function(){
 
     //封装函数change()
 
-    function change(curIndex){
-        for(var i = 0; i<numList.length; i++){
+    function change(curIndex) {
+        for (var i = 0; i < numList.length; i++) {
             PList[i].style.display = 'none';
             numList[i].className = '';
         }
@@ -150,7 +151,7 @@ main.app.imageChange= function(){
 //         })(i)
 //     }
 // }
-main.app.tap = function(){
+main.app.tap = function() {
     var aPull = document.getElementsByClassName('pull');
     var oPullDown = document.getElementById('quick_pullDown');
     var oUl = document.getElementById('four_options');
@@ -161,10 +162,10 @@ main.app.tap = function(){
     // var top1 = 208;
     // var top2 =0;
 
-    for(var i = 0; i<aLi.length; i++){
-        (function(i){
-            aLi[i].onmouseover = function(){
-                for(var j = 0; j<aDiv.length; j++){
+    for (var i = 0; i < aLi.length; i++) {
+        (function(i) {
+            aLi[i].onmouseover = function() {
+                for (var j = 0; j < aDiv.length; j++) {
                     aDiv[j].style.display = 'none';
                 }
                 aDiv[i].style.display = 'block';
@@ -172,17 +173,42 @@ main.app.tap = function(){
         })(i)
     };
 
-    for(var i = 0; i<aPull.length; i++){
-        aVanish[i].onclick = function(){
-            oPullDown.style.top = '208px';
-            onOff = true;
-        }
+    for (var i = 0; i < aPull.length; i++) {
+        aVanish[i].addEventListener('click', function() {
+            if (onff == false) {
+                oPullDown.style.top = '208px';
+                onOff = true;
+                console.log('311');
+            }
+        }, true)
 
-        aPull[i].onmouseover = function(){
-            if(onOff){
+
+
+        aPull[i].onmouseover = function() {
+            if (onOff) {
                 oPullDown.style.top = '0px';
                 onOff = false;
+                console.log('123');
             }
         }
-        }
     }
+
+}
+
+/*上方商品目录选项卡*/
+main.app.productList = function() {
+    var aClassify = document.getElementsByClassName('classify');
+    var aGoodList = document.getElementsByClassName('goodList');
+    for (var i = 0; i < aClassify.length; i++) {
+        (function(i) {
+            aGoodList[i].onmouseover = aClassify[i].onmouseover = function() {
+                aClassify[i].style.display = 'block';
+            }
+            aGoodList[i].onmouseout = aClassify[i].onmouseout = function() {
+                aClassify[i].style.display = 'none';
+            }
+        })(i)
+    }
+}
+
+// 分区内容轮播图
